67038a79c70314ae9cb4f8c28baf5809cbc7f510
jasonzqshen <JasonzqShen@gmail.com>
2012-07-29 02:26:31 +0000

    Test cases for master data is completed

--- test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/MasterDataTestCases.java
+++ test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/MasterDataTestCases.java
@@ -8,11 +8,29 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.jasonzqshen.familyaccounting.core.CoreDriver;
 import com.jasonzqshen.familyaccounting.core.exception.IdentityInvalidChar;
 import com.jasonzqshen.familyaccounting.core.exception.IdentityNoData;
 import com.jasonzqshen.familyaccounting.core.exception.IdentityTooLong;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountNumber;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankKeyMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankKeyMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.BusinessAreaMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.CustomerMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.CustomerMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountGroupMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountGroupMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataBase;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity_GLAccount;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataManagement;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataType;
+import com.jasonzqshen.familyaccounting.core.masterdata.VendorMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.VendorMasterDataFactory;
 
 public class MasterDataTestCases {
 
@@ -26,6 +44,7 @@ public class MasterDataTestCases {
 
 	@Before
 	public void setUp() throws Exception {
+
 	}
 
 	@After
@@ -162,15 +181,191 @@ public class MasterDataTestCases {
 	}
 
 	/**
+	 * test identity
+	 */
+	@Test
+	public void testBankAccountNumber() {
+		// test length
+		StringBuilder builder = new StringBuilder();
+		for (int i = 1; i <= 16; ++i) {
+			builder.append('0');
+			try {
+				char[] testCase = builder.toString().toCharArray();
+				BankAccountNumber test = new BankAccountNumber(testCase);
+				test.toString();
+			} catch (Exception e) {
+				fail(String.format("Length %d: %s", i, e.toString()));
+			}
+		}
+
+		// test number
+		try {
+			char[] testCase = "123456789".toCharArray();
+			BankAccountNumber test = new BankAccountNumber(testCase);
+			assertEquals(test.toString(), "0000000123456789");
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+
+		// test equals
+		try {
+			char[] testCase = "123".toCharArray();
+			BankAccountNumber test1 = new BankAccountNumber(testCase);
+			BankAccountNumber test2 = new BankAccountNumber(testCase);
+			assertEquals(test1, test2);
+			assertEquals(test1.hashCode(), test2.hashCode());
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+
+		// zero length
+		try {
+			char[] zeroTest = "".toCharArray();
+			BankAccountNumber test = new BankAccountNumber(zeroTest);
+			test.toString();
+			fail("IdentityNoData should occur when length is zero");
+		} catch (IdentityNoData e) {
+			// pass
+		} catch (Exception e) {
+			fail("IdentityNoData should occur when length is zero, but it is other exception");
+		}
+
+		// format error
+		try {
+			char[] testCase = "a?".toCharArray();
+			BankAccountNumber test = new BankAccountNumber(testCase);
+			test.toString();
+			fail("IdentityNoData should occur when there is no data");
+		} catch (IdentityInvalidChar e) {
+			// pass
+		} catch (Exception e) {
+			fail("IdentityInvalidChar should occur when length is zero, but it is other exception");
+		}
+
+		// too long
+		try {
+			char[] testCase = "12345678901234567".toCharArray();
+			BankAccountNumber test = new BankAccountNumber(testCase);
+			test.toString();
+			fail("IdentityNoData should occur when there is no data");
+		} catch (IdentityTooLong e) {
+			// pass
+		} catch (Exception e) {
+			fail("IdentityInvalidChar should occur when length is zero, but it is other exception");
+		}
+	}
+
+	/**
 	 * test G/L account master data
 	 */
 	@Test
-	public void testGLAccount() {
+	public void testMasterDataInit() {
 		try {
-			MasterDataIdentity_GLAccount id1 = new MasterDataIdentity_GLAccount(
-					"113100".toCharArray());
-			MasterDataIdentity_GLAccount id1 = new MasterDataIdentity_GLAccount(
-					"113101".toCharArray());
+			CoreDriver coreDriver = CoreDriver.getInstance();
+			MasterDataManagement masterDataManagement = coreDriver
+					.getMasterDataManagement();
+			// initialize
+			masterDataManagement.init();
+			MasterDataBase[] datas;
+
+			// check vendor data
+			VendorMasterDataFactory vendorFactory = (VendorMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.VENDOR);
+			assertEquals(vendorFactory.getMasterDataCount(),
+					TestUtilities.VENDOR_IDS.length);
+			datas = vendorFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				VendorMasterData vendor = (VendorMasterData) data;
+				assertTrue(TestUtilities.containsID(MasterDataType.VENDOR,
+						vendor.getIdentity()));
+				assertTrue(vendor.getDescp() != null);
+			}
+
+			// check customer data
+			CustomerMasterDataFactory customerFactory = (CustomerMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.CUSTOMER);
+			assertEquals(customerFactory.getMasterDataCount(),
+					TestUtilities.CUSTOMER_IDS.length);
+			datas = customerFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				CustomerMasterData customer = (CustomerMasterData) data;
+				assertTrue(TestUtilities.containsID(MasterDataType.CUSTOMER,
+						customer.getIdentity()));
+				assertTrue(customer.getDescp() != null);
+			}
+
+			// check business area
+			CustomerMasterDataFactory businessAreaFactory = (CustomerMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.BUSINESS_AREA);
+			assertEquals(businessAreaFactory.getMasterDataCount(),
+					TestUtilities.BUSINESS_IDS.length);
+			datas = businessAreaFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				BusinessAreaMasterData businessArea = (BusinessAreaMasterData) data;
+				assertTrue(TestUtilities.containsID(
+						MasterDataType.BUSINESS_AREA,
+						businessArea.getIdentity()));
+				assertTrue(businessArea.getDescp() != null);
+				assertTrue(businessArea.getCriticalLevel() != null);
+			}
+
+			// check bank key
+			BankKeyMasterDataFactory bankKeyFactory = (BankKeyMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.BANK_KEY);
+			assertEquals(bankKeyFactory.getMasterDataCount(),
+					TestUtilities.BANK_KEY_IDS.length);
+			datas = bankKeyFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				BankKeyMasterData bankKey = (BankKeyMasterData) data;
+				assertTrue(TestUtilities.containsID(MasterDataType.BANK_KEY,
+						bankKey.getIdentity()));
+				assertTrue(bankKey.getDescp() != null);
+			}
+
+			// check bank account
+			BankAccountMasterDataFactory bankAccountFactory = (BankAccountMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.BANK_KEY);
+			assertEquals(bankAccountFactory.getMasterDataCount(),
+					TestUtilities.BANK_ACCOUNT_IDS.length);
+			datas = bankAccountFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				BankAccountMasterData bankAccount = (BankAccountMasterData) data;
+				assertTrue(TestUtilities.containsID(
+						MasterDataType.BANK_ACCOUNT, bankAccount.getIdentity()));
+				assertTrue(bankAccount.getDescp() != null);
+				assertTrue(bankAccount.getBankAccType() != null);
+				assertTrue(bankAccount.getBankKey() != null);
+				assertTrue(bankAccount.getBankAccountNumber() != null);
+			}
+
+			// check G/L group
+			GLAccountGroupMasterDataFactory accGroupFactory = (GLAccountGroupMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.GL_ACCOUNT_GROUP);
+			assertEquals(accGroupFactory.getMasterDataCount(),
+					TestUtilities.GL_GROUP_IDS.length);
+			datas = accGroupFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				GLAccountGroupMasterData group = (GLAccountGroupMasterData) data;
+				assertTrue(TestUtilities.containsID(
+						MasterDataType.GL_ACCOUNT_GROUP, group.getIdentity()));
+				assertTrue(group.getDescp() != null);
+			}
+
+			// check G/L account
+			GLAccountMasterDataFactory accountFactory = (GLAccountMasterDataFactory) masterDataManagement
+					.getMasterDataFactory(MasterDataType.GL_ACCOUNT);
+			assertEquals(accountFactory.getMasterDataCount(),
+					TestUtilities.GL_GROUP_IDS.length);
+			datas = accountFactory.getAllEntities();
+			for (MasterDataBase data : datas) {
+				GLAccountMasterData account = (GLAccountMasterData) data;
+				assertTrue(TestUtilities.containsID(MasterDataType.GL_ACCOUNT,
+						account.getIdentity()));
+				assertTrue(account.getDescp() != null);
+				assertTrue(account.getAccountGroup() != null);
+				assertTrue(account.getGLAccountType() != null);
+			}
+
 		} catch (Exception e) {
 			fail(e.toString());
 		}
