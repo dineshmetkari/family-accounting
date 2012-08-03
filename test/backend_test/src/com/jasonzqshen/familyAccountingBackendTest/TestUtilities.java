d165abf0a1e6a92750daa40b7321b10f5f600089
jasonzqshen <JasonzqShen@gmail.com>
2012-08-03 15:14:49 +0000

    transaction data loading test cases is completed

--- test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/TestUtilities.java
+++ test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/TestUtilities.java
@@ -1,8 +1,46 @@
 package com.jasonzqshen.familyAccountingBackendTest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import com.jasonzqshen.familyaccounting.core.CoreDriver;
+import com.jasonzqshen.familyaccounting.core.exception.FiscalMonthRangeException;
+import com.jasonzqshen.familyaccounting.core.exception.FiscalYearRangeException;
+import com.jasonzqshen.familyaccounting.core.exception.IdentityInvalidChar;
+import com.jasonzqshen.familyaccounting.core.exception.IdentityNoData;
+import com.jasonzqshen.familyaccounting.core.exception.IdentityTooLong;
+import com.jasonzqshen.familyaccounting.core.exception.MasterDataIdentityExists;
+import com.jasonzqshen.familyaccounting.core.exception.MasterDataIdentityNotDefined;
+import com.jasonzqshen.familyaccounting.core.exception.NoMasterDataFactoryClass;
+import com.jasonzqshen.familyaccounting.core.exception.ParametersException;
+import com.jasonzqshen.familyaccounting.core.exception.RootFolderNotExsits;
+import com.jasonzqshen.familyaccounting.core.exception.SystemException;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankAccountNumber;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankKeyMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.BankKeyMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.BusinessAreaMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.BusinessAreaMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.CustomerMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.CustomerMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountGroupMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountGroupMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.GLAccountMasterDataFactory;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity_GLAccount;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataManagement;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataType;
+import com.jasonzqshen.familyaccounting.core.masterdata.VendorMasterData;
+import com.jasonzqshen.familyaccounting.core.masterdata.VendorMasterDataFactory;
+import com.jasonzqshen.familyaccounting.core.transaction.MonthIdentity;
+import com.jasonzqshen.familyaccounting.core.transaction.TransactionDataManagement;
 import com.jasonzqshen.familyaccounting.core.utils.BankAccountType;
+import com.jasonzqshen.familyaccounting.core.utils.CoreMessage;
 import com.jasonzqshen.familyaccounting.core.utils.CriticalLevel;
 import com.jasonzqshen.familyaccounting.core.utils.GLAccountType;
 
@@ -19,6 +57,8 @@ public class TestUtilities {
 	public static final String TEST_BANK_KEY_ID = "CMB_6620";
 	public static final String TEST_GL_ACCOUNT_GROUP = "1010";
 	public static final GLAccountType TEST_GL_ACCOUNT_TYPE = GLAccountType.BALANCE;
+	public static final String TEST_DOC_NUM = "1000000000";
+	public static final String TEST_DOC_ID = "1000000000_2012_07";
 
 	public static final String TEST_DESCP = "test";
 
@@ -38,6 +78,13 @@ public class TestUtilities {
 			"1070", "1400", "1500", "1510", "1520", "1550", "2480", "2708",
 			"2740", "4000", "4020", "4060", "4080", "5000", "5001", "5002",
 			"5003" };
+	public static final String[] DOCUMNET_NUMS = { "1000000001", "1000000002" };
+	public static final String GL_ACCOUNT1 = "0000406002";
+	public static final String GL_ACCOUNT2 = "0000140001";
+	public static final double AMOUNT = 100;
+	public static final String VENDOR = "BUS";
+	public static final String CUSTOMER = "MS";
+	public static final String BUSINESS_AREA = "WORK";
 
 	/**
 	 * 
@@ -75,5 +122,269 @@ public class TestUtilities {
 
 		}
 		return false;
+	}
+
+	/**
+	 * clear test root folder
+	 */
+	public static File clearTestingRootFolder() {
+		// set the root folder
+		File rootFolder = new File(TestUtilities.TEST_ROOT_FOLDER_EMPTY);
+		if (!rootFolder.exists()) {
+			rootFolder.mkdir();
+		}
+
+		// clear the master data folder
+		File masterDataFolder = new File(String.format("%s/%s",
+				TestUtilities.TEST_ROOT_FOLDER_EMPTY,
+				MasterDataManagement.MASTER_DATA_FOLDER));
+		if (masterDataFolder.exists()) {
+			for (File f : masterDataFolder.listFiles()) {
+				f.delete();
+			}
+		}
+
+		// clear transaction folder
+		File tranDataFolder = new File(String.format("%s/%s",
+				TestUtilities.TEST_ROOT_FOLDER_EMPTY,
+				TransactionDataManagement.TRANSACTION_DATA_FOLDER));
+		if (tranDataFolder.exists()) {
+			for (File f : masterDataFolder.listFiles()) {
+				f.delete();
+			}
+		}
+		return rootFolder;
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws RootFolderNotExsits
+	 * @throws SystemException
+	 * @throws NoMasterDataFactoryClass
+	 * @throws IdentityInvalidChar
+	 * @throws IdentityNoData
+	 * @throws IdentityTooLong
+	 * @throws MasterDataIdentityExists
+	 * @throws ParametersException
+	 * @throws MasterDataIdentityNotDefined
+	 * @throws FiscalMonthRangeException
+	 * @throws FiscalYearRangeException
+	 */
+	public static CoreDriver establishMasterData(ArrayList<CoreMessage> messages)
+			throws NoMasterDataFactoryClass, SystemException,
+			RootFolderNotExsits, IdentityTooLong, IdentityNoData,
+			IdentityInvalidChar, ParametersException, MasterDataIdentityExists,
+			MasterDataIdentityNotDefined, FiscalYearRangeException,
+			FiscalMonthRangeException {
+		CoreDriver coreDriver = CoreDriver.getInstance();
+
+		// set root path
+		coreDriver.setRootPath(TestUtilities.TEST_ROOT_FOLDER_EMPTY);
+		coreDriver.setStartMonthID(new MonthIdentity(2012, 7));
+
+		coreDriver.init(messages);
+		if (messages.size() > 0) {
+			for (CoreMessage m : messages) {
+				System.out.println(m.toString());
+			}
+		}
+
+		/**
+		 * check the factory is initialized, and the factory with no master data
+		 * entities
+		 */
+		MasterDataManagement masterDataManagement = coreDriver
+				.getMasterDataManagement();
+
+		// vendor
+		VendorMasterDataFactory vendorFactory = (VendorMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.VENDOR);
+		assertEquals(0, vendorFactory.getMasterDataCount());
+		// customer
+		CustomerMasterDataFactory customerFactory = (CustomerMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.CUSTOMER);
+		assertEquals(0, customerFactory.getMasterDataCount());
+		// business area
+		BusinessAreaMasterDataFactory businessFactory = (BusinessAreaMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.BUSINESS_AREA);
+		assertEquals(0, businessFactory.getMasterDataCount());
+		// bank key
+		BankKeyMasterDataFactory bankKeyFactory = (BankKeyMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.BANK_KEY);
+		assertEquals(0, bankKeyFactory.getMasterDataCount());
+		// bank account
+		BankAccountMasterDataFactory bankAccountFactory = (BankAccountMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.BANK_ACCOUNT);
+		assertEquals(0, bankAccountFactory.getMasterDataCount());
+		// GL account group
+		GLAccountGroupMasterDataFactory groupFactory = (GLAccountGroupMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.GL_ACCOUNT_GROUP);
+		assertEquals(0, groupFactory.getMasterDataCount());
+		// GL account
+		GLAccountMasterDataFactory accountFactory = (GLAccountMasterDataFactory) masterDataManagement
+				.getMasterDataFactory(MasterDataType.GL_ACCOUNT);
+		assertEquals(0, accountFactory.getMasterDataCount());
+
+		/** add master data entities */
+		// vendor
+		for (String str : TestUtilities.VENDOR_IDS) {
+			VendorMasterData vendor = (VendorMasterData) vendorFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP);
+			assertTrue(vendor != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.VENDOR_IDS) {
+			try {
+				VendorMasterData vendor = (VendorMasterData) vendorFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+
+			}
+		}
+
+		// customer
+		for (String str : TestUtilities.CUSTOMER_IDS) {
+			CustomerMasterData customer = (CustomerMasterData) customerFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP);
+			assertTrue(customer != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.CUSTOMER_IDS) {
+			try {
+				CustomerMasterData customer = (CustomerMasterData) customerFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+
+		// bank key
+		for (String str : TestUtilities.BANK_KEY_IDS) {
+			BankKeyMasterData bankKey = (BankKeyMasterData) bankKeyFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP);
+			assertTrue(bankKey != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.BANK_KEY_IDS) {
+			try {
+				BankKeyMasterData bankKey = (BankKeyMasterData) bankKeyFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+
+		// bank account
+		for (String str : TestUtilities.BANK_ACCOUNT_IDS) {
+			MasterDataIdentity bankKey = new MasterDataIdentity(
+					TestUtilities.TEST_BANK_KEY.toCharArray());
+			BankAccountNumber accountNum = new BankAccountNumber(
+					TestUtilities.TEST_ACCOUNT_NUMBER.toCharArray());
+			BankAccountMasterData vendor = (BankAccountMasterData) bankAccountFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP, accountNum, bankKey,
+							BankAccountType.SAVING_ACCOUNT);
+			assertTrue(vendor != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.BANK_ACCOUNT_IDS) {
+			try {
+				MasterDataIdentity bankKey = new MasterDataIdentity(
+						TestUtilities.TEST_BANK_KEY.toCharArray());
+				BankAccountNumber accountNum = new BankAccountNumber(
+						TestUtilities.TEST_ACCOUNT_NUMBER.toCharArray());
+				BankAccountMasterData vendor = (BankAccountMasterData) bankAccountFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP, accountNum, bankKey,
+								TestUtilities.TEST_BANK_ACCOUNT_TYPE);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+
+		// business area
+		for (String str : TestUtilities.BUSINESS_IDS) {
+			BusinessAreaMasterData businessArea = (BusinessAreaMasterData) businessFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP,
+							TestUtilities.TEST_CRITICAL_LEVEL);
+			assertTrue(businessArea != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.BUSINESS_IDS) {
+			try {
+				BusinessAreaMasterData businessArea = (BusinessAreaMasterData) businessFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP,
+								TestUtilities.TEST_CRITICAL_LEVEL);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+
+		// G/L account group
+		for (String str : TestUtilities.GL_GROUP_IDS) {
+			GLAccountGroupMasterData group = (GLAccountGroupMasterData) groupFactory
+					.createNewMasterDataBase(
+							new MasterDataIdentity(str.toCharArray()),
+							TestUtilities.TEST_DESCP);
+			assertTrue(group != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.GL_GROUP_IDS) {
+			try {
+				GLAccountGroupMasterData group = (GLAccountGroupMasterData) groupFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity(str.toCharArray()),
+								TestUtilities.TEST_DESCP);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+
+		// G/L account
+		for (String str : TestUtilities.GL_IDS) {
+			MasterDataIdentity group = new MasterDataIdentity(
+					TestUtilities.TEST_GL_ACCOUNT_GROUP.toCharArray());
+			GLAccountMasterData glAccount = (GLAccountMasterData) accountFactory
+					.createNewMasterDataBase(new MasterDataIdentity_GLAccount(
+							str.toCharArray()), TestUtilities.TEST_DESCP,
+							TestUtilities.TEST_GL_ACCOUNT_TYPE, group);
+			assertTrue(glAccount != null);
+		}
+		// duplicate id
+		for (String str : TestUtilities.GL_IDS) {
+			try {
+				MasterDataIdentity group = new MasterDataIdentity(
+						TestUtilities.TEST_GL_ACCOUNT_GROUP.toCharArray());
+				GLAccountMasterData glAccount = (GLAccountMasterData) accountFactory
+						.createNewMasterDataBase(
+								new MasterDataIdentity_GLAccount(str
+										.toCharArray()),
+								TestUtilities.TEST_DESCP,
+								TestUtilities.TEST_GL_ACCOUNT_TYPE, group);
+				assertTrue(false);
+			} catch (MasterDataIdentityExists e) {
+			}
+		}
+		return coreDriver;
 	}
 }
