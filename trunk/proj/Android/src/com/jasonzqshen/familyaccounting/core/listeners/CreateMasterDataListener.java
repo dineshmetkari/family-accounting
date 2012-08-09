4451df341dd68896293723a1f801f4f8993cd3b0
JasonzqShen@gmail.com <JasonzqShen@gmail.com@4088d752-dbed-ae4f-5ba4-2e21a2153df2>
2012-08-09 10:45:57 +0000

    delete all contents in android application for re-create

--- trunk/proj/Android/src/com/jasonzqshen/familyaccounting/core/listeners/CreateMasterDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.jasonzqshen.familyaccounting.core.listeners;
-
-import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataBase;
-import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataFactoryBase;
-
-public interface CreateMasterDataListener {
-	void onCreateMasterDataListener(MasterDataFactoryBase factory, MasterDataBase master);
-}
--- trunk/proj/Android/src/com/jasonzqshen/familyaccounting/core/listeners/LoadDocumentListener.java
+++
@@ -1,7 +0,0 @@
-package com.jasonzqshen.familyaccounting.core.listeners;
-
-import com.jasonzqshen.familyaccounting.core.transaction.HeadEntity;
-
-public interface LoadDocumentListener{
-	void onLoadDocumentListener(Object source, HeadEntity document);
-}
