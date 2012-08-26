29f2199af6aee02b8b2d9307116ac9f73e30d994
jasonzqshen@gmail.com <jasonzqshen@gmail.com@4088d752-dbed-ae4f-5ba4-2e21a2153df2>
2012-08-26 14:05:43 +0000

    fix bugs in document entries

--- trunk/proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/masterdata/MasterDataFactoryBase.java
+++ trunk/proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/masterdata/MasterDataFactoryBase.java
@@ -21,147 +21,153 @@ import com.jasonzqshen.familyaccounting.core.utils.XMLTransfer;
  * 
  */
 public abstract class MasterDataFactoryBase {
-	protected final CoreDriver _coreDriver;
-	protected final MasterDataManagement _management;
-	protected final Hashtable<MasterDataIdentity, MasterDataBase> _list;
-	boolean _containDirtyData;
+    protected final CoreDriver _coreDriver;
 
-	/**
-	 * constructor
-	 * 
-	 * @param parser
-	 * @param coreDriver
-	 */
-	protected MasterDataFactoryBase(CoreDriver coreDriver,
-			MasterDataManagement management) {
-		_coreDriver = coreDriver;
-		_management = management;
-		_list = new Hashtable<MasterDataIdentity, MasterDataBase>();
-		_containDirtyData = false;
-	}
+    protected final MasterDataManagement _management;
 
-	/**
-	 * create new master data with id. And add the master data into list
-	 * 
-	 * @param id
-	 * @return
-	 * @throws ParametersException
-	 * @throws MasterDataIdentityExists
-	 * @throws MasterDataIdentityNotDefined
-	 */
-	public abstract MasterDataBase createNewMasterDataBase(
-			MasterDataIdentity identity, String descp, Object... objects)
-			throws ParametersException, MasterDataIdentityExists,
-			MasterDataIdentityNotDefined, SystemException;
+    protected final Hashtable<MasterDataIdentity, MasterDataBase> _list;
 
-	/**
-	 * parse master data
-	 * 
-	 * @param coreDriver
-	 * @param elem
-	 * @return
-	 * @throws MasterDataFileFormatException
-	 */
-	public abstract MasterDataBase parseMasterData(CoreDriver coreDriver,
-			Element elem) throws MasterDataFileFormatException;
+    boolean _containDirtyData;
 
-	/**
-	 * remove the master data entity from list
-	 * 
-	 * @param id
-	 * @return
-	 */
-	public void removeNewMasterDataBase(MasterDataIdentity id) {
-		_list.remove(id);
-	}
+    /**
+     * constructor
+     * 
+     * @param parser
+     * @param coreDriver
+     */
+    protected MasterDataFactoryBase(CoreDriver coreDriver,
+            MasterDataManagement management) {
+        _coreDriver = coreDriver;
+        _management = management;
+        _list = new Hashtable<MasterDataIdentity, MasterDataBase>();
+        _containDirtyData = false;
+    }
 
-	/**
-	 * parse memory to XML
-	 * 
-	 * @return
-	 */
-	public String toXmlDocument() {
+    /**
+     * create new master data with id. And add the master data into list
+     * 
+     * @param id
+     * @return
+     * @throws ParametersException
+     * @throws MasterDataIdentityExists
+     * @throws MasterDataIdentityNotDefined
+     */
+    public abstract MasterDataBase createNewMasterDataBase(
+            MasterDataIdentity identity, String descp, Object... objects)
+            throws ParametersException, MasterDataIdentityExists,
+            MasterDataIdentityNotDefined, SystemException;
 
-		if (_list.size() == 0) {
-			return String.format("%s%s %s", XMLTransfer.SINGLE_TAG_LEFT,
-					MasterDataUtils.XML_ROOT, XMLTransfer.SINGLE_TAG_RIGHT);
-		}
+    /**
+     * parse master data
+     * 
+     * @param coreDriver
+     * @param elem
+     * @return
+     * @throws MasterDataFileFormatException
+     */
+    public abstract MasterDataBase parseMasterData(CoreDriver coreDriver,
+            Element elem) throws MasterDataFileFormatException;
 
-		StringBuilder ret = new StringBuilder();
-		// begin tag
-		ret.append(String.format("%s%s %s", XMLTransfer.BEGIN_TAG_LEFT,
-				MasterDataUtils.XML_ROOT, XMLTransfer.BEGIN_TAG_RIGHT));
+    /**
+     * remove the master data entity from list
+     * 
+     * @param id
+     * @return
+     */
+    public void removeNewMasterDataBase(MasterDataIdentity id) {
+        _list.remove(id);
+    }
 
-		for (MasterDataBase masterData : _list.values()) {
-			ret.append(String.format("\t%s%s ", XMLTransfer.SINGLE_TAG_LEFT,
-					MasterDataUtils.XML_ENTITY));
+    /**
+     * parse memory to XML
+     * 
+     * @return
+     */
+    public String toXmlDocument() {
 
-			String str = masterData.toXML();
-			ret.append(str);
+        if (_list.size() == 0) {
+            return String.format("%s%s %s", XMLTransfer.SINGLE_TAG_LEFT,
+                    MasterDataUtils.XML_ROOT, XMLTransfer.SINGLE_TAG_RIGHT);
+        }
 
-			ret.append(XMLTransfer.SINGLE_TAG_RIGHT);
-		}
+        StringBuilder ret = new StringBuilder();
+        // begin tag
+        ret.append(String.format("%s%s %s", XMLTransfer.BEGIN_TAG_LEFT,
+                MasterDataUtils.XML_ROOT, XMLTransfer.BEGIN_TAG_RIGHT));
 
-		// end tag
-		ret.append(String.format("%s%s %s", XMLTransfer.END_TAG_LEFT,
-				MasterDataUtils.XML_ROOT, XMLTransfer.END_TAG_RIGHT));
+        ArrayList<MasterDataBase> list = new ArrayList<MasterDataBase>(
+                _list.values());
+        Collections.sort(list);
+        for (MasterDataBase masterData : list) {
+            ret.append(String.format("\t%s%s ", XMLTransfer.SINGLE_TAG_LEFT,
+                    MasterDataUtils.XML_ENTITY));
 
-		return ret.toString();
-	}
+            String str = masterData.toXML();
+            ret.append(str);
 
-	/**
-	 * get the count of master data
-	 * 
-	 * @return
-	 */
-	public int getMasterDataCount() {
-		return _list.size();
-	}
+            ret.append(XMLTransfer.SINGLE_TAG_RIGHT);
+        }
 
-	/**
-	 * get master data entities
-	 * 
-	 * @return
-	 */
-	public MasterDataBase[] getAllEntities() {
-		ArrayList<MasterDataBase> collection = new ArrayList<MasterDataBase>();
+        // end tag
+        ret.append(String.format("%s%s %s", XMLTransfer.END_TAG_LEFT,
+                MasterDataUtils.XML_ROOT, XMLTransfer.END_TAG_RIGHT));
 
-		for (MasterDataBase data : _list.values()) {
-			collection.add(data);
-		}
-		Collections.sort(collection);
+        return ret.toString();
+    }
 
-		MasterDataBase[] ret = new MasterDataBase[getMasterDataCount()];
-		int index = 0;
-		for (MasterDataBase data : collection) {
-			ret[index++] = data;
-		}
+    /**
+     * get the count of master data
+     * 
+     * @return
+     */
+    public int getMasterDataCount() {
+        return _list.size();
+    }
 
-		return ret;
-	}
+    /**
+     * get master data entities
+     * 
+     * @return
+     */
+    public MasterDataBase[] getAllEntities() {
+        ArrayList<MasterDataBase> collection = new ArrayList<MasterDataBase>();
 
-	/**
-	 * get master data entity according identity
-	 * 
-	 * @param id
-	 *            identity
-	 * @return master data entity
-	 */
-	public MasterDataBase getEntity(MasterDataIdentity id) {
-		if (!contains(id)) {
-			return null;
-		}
-		return _list.get(id);
-	}
+        for (MasterDataBase data : _list.values()) {
+            collection.add(data);
+        }
+        Collections.sort(collection);
 
-	/**
-	 * contains value with master data identity
-	 * 
-	 * @param id
-	 * @return
-	 */
-	public boolean contains(MasterDataIdentity id) {
-		return _list.containsKey(id);
-	}
+        MasterDataBase[] ret = new MasterDataBase[getMasterDataCount()];
+        int index = 0;
+        for (MasterDataBase data : collection) {
+            ret[index++] = data;
+        }
+
+        return ret;
+    }
+
+    /**
+     * get master data entity according identity
+     * 
+     * @param id
+     *            identity
+     * @return master data entity
+     */
+    public MasterDataBase getEntity(MasterDataIdentity id) {
+        if (!contains(id)) {
+            return null;
+        }
+        return _list.get(id);
+    }
+
+    /**
+     * contains value with master data identity
+     * 
+     * @param id
+     * @return
+     */
+    public boolean contains(MasterDataIdentity id) {
+        return _list.containsKey(id);
+    }
 
 }
