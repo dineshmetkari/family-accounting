9b009acd1a1d536a68be5fdfd02556e97250f24e
jasonzqshen <JasonzqShen@gmail.com>
2012-08-27 04:10:12 +0000

    add tester for document index on business area

---
+++ test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/reports/DocumentBusinessIndexTester.java
@@ -0,0 +1,58 @@
+package com.jasonzqshen.familyAccountingBackendTest.reports;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import com.jasonzqshen.familyAccountingBackendTest.utils.TestUtilities;
+import com.jasonzqshen.familyAccountingBackendTest.utils.TesterBase;
+import com.jasonzqshen.familyaccounting.core.CoreDriver;
+import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity;
+import com.jasonzqshen.familyaccounting.core.reports.DocumentBusinessIndex;
+import com.jasonzqshen.familyaccounting.core.reports.DocumentIndex;
+import com.jasonzqshen.familyaccounting.core.reports.DocumentIndexItemWithBalance;
+import com.jasonzqshen.familyaccounting.core.reports.ReportsManagement;
+import com.jasonzqshen.familyaccounting.core.transaction.HeadEntity;
+import com.jasonzqshen.familyaccounting.core.utils.CurrencyAmount;
+
+public class DocumentBusinessIndexTester extends TesterBase {
+    private ReportsManagement _reportsManagement;
+
+    @Override
+    protected void doTest(CoreDriver coreDriver) throws Exception {
+        _reportsManagement = new ReportsManagement(coreDriver);
+
+        // set root path
+        coreDriver.setRootPath(TestUtilities.TEST_ROOT_FOLDER);
+    }
+
+    @Override
+    protected void check(CoreDriver coreDriver) throws Exception {
+        DocumentBusinessIndex index = (DocumentBusinessIndex) _reportsManagement
+                .getDocumentIndex(DocumentIndex.BUSINESS_INDEX);
+        assertEquals(1, index.getKeys().size());
+        MasterDataIdentity id = new MasterDataIdentity(
+                TestUtilities.BUSINESS_AREA);
+        DocumentIndexItemWithBalance item = index.getIndexItem(id);
+        ArrayList<HeadEntity> entities = item.getEntities();
+        assertEquals(3, entities.size());
+
+        entities = item.getEntities(coreDriver.getCurMonthId(),
+                coreDriver.getCurMonthId());
+        assertEquals(2, entities.size());
+
+        CurrencyAmount amount1 = CurrencyAmount
+                .parse(TestUtilities.TEST_AMOUNT1);
+        assertEquals(amount1, item.getAmountSum());
+
+        assertEquals(
+                new CurrencyAmount(),
+                item.getAmount(coreDriver.getCurMonthId(),
+                        coreDriver.getCurMonthId()));
+        assertEquals(
+                amount1,
+                item.getAmount(coreDriver.getStartMonthId(),
+                        coreDriver.getStartMonthId()));
+    }
+
+}
