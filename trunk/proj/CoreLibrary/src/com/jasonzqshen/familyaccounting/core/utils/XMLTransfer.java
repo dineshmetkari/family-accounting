a051a5c27dee036ade0c0df91436c0ed608ee46c
RayZhou.ECNU@gmail.com <RayZhou.ECNU@gmail.com@4088d752-dbed-ae4f-5ba4-2e21a2153df2>
2012-09-30 20:07:35 +0000

    add toSafeString function replacing all invalid chars

--- trunk/proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/utils/XMLTransfer.java
+++ trunk/proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/utils/XMLTransfer.java
@@ -17,8 +17,24 @@ public class XMLTransfer {
 	public static final String END_TAG_RIGHT = ">\n";
 	public static final String SINGLE_TAG_LEFT = "<";
 	public static final String SINGLE_TAG_RIGHT = "/>\n";
+	
+	public static final String char_amp = "&amp;";
+	public static final String char_apos = "&apos;";
+	public static final String char_quot = "&quot;";
 
 	public static boolean validCharacters(String str) {
 		return true;
+	}
+	
+	public static String toSafeString(String str) {
+		
+		String rs = str;
+		
+		rs = rs.replaceAll("&", char_amp);
+		rs = rs.replaceAll("'", char_apos);
+		rs = rs.replaceAll("\"", char_quot);		
+		
+		return rs;
+		
 	}
 }
