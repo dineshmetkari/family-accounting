a7147d6900a71eedccc3ba8af0642ffc4ce964fd
jasonzqshen <JasonzqShen@gmail.com>
2012-10-03 10:27:23 +0000

    Add new functionality in DocumentIndex class. User can get the summary amount of the DocumentIndex

--- proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/reports/DocumentBusinessIndex.java
+++ proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/reports/DocumentBusinessIndex.java
@@ -5,6 +5,8 @@ import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataType;
 import com.jasonzqshen.familyaccounting.core.transaction.HeadEntity;
 import com.jasonzqshen.familyaccounting.core.transaction.ItemEntity;
+import com.jasonzqshen.familyaccounting.core.utils.CreditDebitIndicator;
+import com.jasonzqshen.familyaccounting.core.utils.CurrencyAmount;
 
 /**
  * document index based on business area
@@ -14,32 +16,39 @@ import com.jasonzqshen.familyaccounting.core.transaction.ItemEntity;
  */
 public class DocumentBusinessIndex extends DocumentIndex {
 
-    protected DocumentBusinessIndex(CoreDriver coreDriver) {
-        super(coreDriver);
-    }
+	protected DocumentBusinessIndex(CoreDriver coreDriver) {
+		super(coreDriver);
+	}
 
-    @Override
-    protected void newDoc(HeadEntity head) {
-        ItemEntity[] items = head.getItems();
-        for (int i = 0; i < items.length; ++i) {
-            MasterDataIdentity id = items[i].getBusinessArea();
+	@Override
+	protected void newDoc(HeadEntity head) {
+		ItemEntity[] items = head.getItems();
+		for (int i = 0; i < items.length; ++i) {
+			MasterDataIdentity id = items[i].getBusinessArea();
 
-            if (id != null) {
-                DocumentIndexItem item;
-                if (!_list.containsKey(id)) {
-                    item = new DocumentIndexItemWithBalance(_coreDriver, id,
-                            MasterDataType.BUSINESS_AREA);
-                    _list.put(id, item);
-                } else {
-                    item = _list.get(id);
-                }
-                item.addDoc(head);
-            }
-        }
-    }
+			if (id != null) {
+				DocumentIndexItem item;
+				if (!_list.containsKey(id)) {
+					item = new DocumentIndexItemWithBalance(_coreDriver, id,
+							MasterDataType.BUSINESS_AREA);
+					_list.put(id, item);
+				} else {
+					item = _list.get(id);
+				}
+				// add document
+				item.addDoc(head);
+				// add amount
+				CurrencyAmount amount = items[i].getAmount();
+				if (items[i].getCDIndicator() == CreditDebitIndicator.CREDIT) {
+					amount.negate();
+				}
+				item.addAmount(head.getMonthId(), amount);
+			}
+		}
+	}
 
-    @Override
-    public DocumentIndexItemWithBalance getIndexItem(MasterDataIdentity key) {
-        return (DocumentIndexItemWithBalance) super.getIndexItem(key);
-    }
+	@Override
+	public DocumentIndexItemWithBalance getIndexItem(MasterDataIdentity key) {
+		return (DocumentIndexItemWithBalance) super.getIndexItem(key);
+	}
 }
--- proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/reports/DocumentIndexItem.java
+++ proj/CoreLibrary/src/com/jasonzqshen/familyaccounting/core/reports/DocumentIndexItem.java
@@ -3,121 +3,171 @@ package com.jasonzqshen.familyaccounting.core.reports;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Hashtable;
 
 import com.jasonzqshen.familyaccounting.core.CoreDriver;
 import com.jasonzqshen.familyaccounting.core.masterdata.MasterDataIdentity;
 import com.jasonzqshen.familyaccounting.core.transaction.HeadEntity;
 import com.jasonzqshen.familyaccounting.core.transaction.MonthIdentity;
+import com.jasonzqshen.familyaccounting.core.utils.CurrencyAmount;
 
 /**
  * document index item is based on the each master data
  * 
- * @author Jason
+ * @author jasonzqshen@gmail.com
  * 
  */
 public class DocumentIndexItem {
-    protected final ArrayList<HeadEntity> _list;
+	protected final ArrayList<HeadEntity> _list;
 
-    protected final MasterDataIdentity _id;
+	protected final Hashtable<MonthIdentity, CurrencyAmount> _amountList;
 
-    protected final CoreDriver _coreDriver;
+	protected final MasterDataIdentity _id;
 
-    DocumentIndexItem(CoreDriver coreDriver, MasterDataIdentity id) {
-        _coreDriver = coreDriver;
-        _id = id;
-        _list = new ArrayList<HeadEntity>();
-    }
+	protected final CoreDriver _coreDriver;
 
-    /**
-     * get entities based on the comparator
-     * 
-     * @param comparator
-     * @return
-     */
-    public ArrayList<HeadEntity> getEntities(Comparator<HeadEntity> comparator) {
-        ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>(_list);
-        Collections.sort(ret, comparator);
+	DocumentIndexItem(CoreDriver coreDriver, MasterDataIdentity id) {
+		_coreDriver = coreDriver;
+		_id = id;
+		_list = new ArrayList<HeadEntity>();
+		_amountList = new Hashtable<MonthIdentity, CurrencyAmount>();
+	}
 
-        return ret;
-    }
+	/**
+	 * get entities based on the comparator
+	 * 
+	 * @param comparator
+	 * @return
+	 */
+	public ArrayList<HeadEntity> getEntities(Comparator<HeadEntity> comparator) {
+		ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>(_list);
+		Collections.sort(ret, comparator);
 
-    /**
-     * get entities with default order, sorted based on Date
-     * 
-     * @return
-     */
-    public ArrayList<HeadEntity> getEntities() {
-        ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>(_list);
-        return ret;
-    }
+		return ret;
+	}
 
-    /**
-     * get month identity
-     * 
-     * @param startId
-     * @param endId
-     * @return
-     */
-    public ArrayList<HeadEntity> getEntities(MonthIdentity startId,
-            MonthIdentity endId) {
-        if (startId == null) {
-            startId = _coreDriver.getStartMonthId();
-        }
-        if (endId == null) {
-            endId = _coreDriver.getCurMonthId();
-        }
-        ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>();
-        for (HeadEntity head : _list) {
-            if (head.getMonthId().compareTo(endId) > 0) {
-                break;
-            }
+	/**
+	 * get entities with default order, sorted based on Date
+	 * 
+	 * @return
+	 */
+	public ArrayList<HeadEntity> getEntities() {
+		ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>(_list);
+		return ret;
+	}
 
-            if (head.getMonthId().compareTo(startId) >= 0) {
-                ret.add(head);
-            }
+	/**
+	 * get month identity
+	 * 
+	 * @param startId
+	 * @param endId
+	 * @return
+	 */
+	public ArrayList<HeadEntity> getEntities(MonthIdentity startId,
+			MonthIdentity endId) {
+		if (startId == null) {
+			startId = _coreDriver.getStartMonthId();
+		}
+		if (endId == null) {
+			endId = _coreDriver.getCurMonthId();
+		}
+		ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>();
+		for (HeadEntity head : _list) {
+			if (head.getMonthId().compareTo(endId) > 0) {
+				break;
+			}
 
-        }
+			if (head.getMonthId().compareTo(startId) >= 0) {
+				ret.add(head);
+			}
 
-        return ret;
-    }
+		}
 
-    /**
-     * get month identity
-     * 
-     * @param startId
-     * @param endId
-     * @return
-     */
-    public ArrayList<HeadEntity> getEntities(Comparator<HeadEntity> comparator,
-            MonthIdentity startId, MonthIdentity endId) {
-        if (startId == null) {
-            startId = _coreDriver.getStartMonthId();
-        }
-        if (endId == null) {
-            endId = _coreDriver.getCurMonthId();
-        }
-        ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>();
-        for (HeadEntity head : _list) {
-            if (head.getMonthId().compareTo(startId) >= 0) {
-                ret.add(head);
-            }
-            if (head.getMonthId().compareTo(endId) > 0) {
-                break;
-            }
-        }
+		return ret;
+	}
 
-        Collections.sort(ret, comparator);
+	/**
+	 * get month identity
+	 * 
+	 * @param startId
+	 * @param endId
+	 * @return
+	 */
+	public ArrayList<HeadEntity> getEntities(Comparator<HeadEntity> comparator,
+			MonthIdentity startId, MonthIdentity endId) {
+		if (startId == null) {
+			startId = _coreDriver.getStartMonthId();
+		}
+		if (endId == null) {
+			endId = _coreDriver.getCurMonthId();
+		}
+		ArrayList<HeadEntity> ret = new ArrayList<HeadEntity>();
+		for (HeadEntity head : _list) {
+			if (head.getMonthId().compareTo(startId) >= 0) {
+				ret.add(head);
+			}
+			if (head.getMonthId().compareTo(endId) > 0) {
+				break;
+			}
+		}
 
-        return ret;
-    }
+		Collections.sort(ret, comparator);
 
-    /**
-     * add document
-     * 
-     * @param head
-     */
-    void addDoc(HeadEntity head) {
-        _list.add(head);
-        Collections.sort(_list, DocumentIndex.COMPARATOR_DATE);
-    }
+		return ret;
+	}
+
+	/**
+	 * get all amount
+	 * 
+	 * @return
+	 */
+	public CurrencyAmount getAmount() {
+		CurrencyAmount sum = new CurrencyAmount();
+		for (CurrencyAmount amount : _amountList.values()) {
+			sum.addTo(amount);
+		}
+
+		return sum;
+	}
+
+	/**
+	 * get amount
+	 * 
+	 * @param monthID
+	 * @return
+	 */
+	public CurrencyAmount getAmount(MonthIdentity monthID) {
+		CurrencyAmount sum = new CurrencyAmount();
+		if (_amountList.containsKey(monthID)) {
+			sum.addTo(_amountList.get(monthID));
+		}
+
+		return sum;
+	}
+
+	/**
+	 * add document
+	 * 
+	 * @param head
+	 */
+	void addDoc(HeadEntity head) {
+		_list.add(head);
+		Collections.sort(_list, DocumentIndex.COMPARATOR_DATE);
+	}
+
+	/**
+	 * add currency amount
+	 * 
+	 * @param monthId
+	 * @param amount
+	 */
+	void addAmount(MonthIdentity monthId, CurrencyAmount amount) {
+		if (_amountList.containsKey(monthId)) {
+			CurrencyAmount v1 = _amountList.get(monthId);
+			v1.addTo(amount);
+			return;
+		}
+		// put
+		_amountList.put(monthId, amount);
+	}
 }
