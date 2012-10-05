c719acf8351af83de6122c4a2af14824c5c2c379
jasonzqshen@gmail.com <jasonzqshen@gmail.com@4088d752-dbed-ae4f-5ba4-2e21a2153df2>
2012-10-05 02:39:14 +0000

    DocumentAccountIndexTester.class: add amount testing AccountBalanceReportTester.class: add cost account testing

--- trunk/test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/reports/DocumentAccountIndexTester.java
+++ trunk/test/backend_test/src/com/jasonzqshen/familyAccountingBackendTest/reports/DocumentAccountIndexTester.java
@@ -13,23 +13,25 @@ import com.jasonzqshen.familyaccounting.core.reports.DocumentIndex;
 import com.jasonzqshen.familyaccounting.core.reports.DocumentIndexItem;
 import com.jasonzqshen.familyaccounting.core.reports.ReportsManagement;
 import com.jasonzqshen.familyaccounting.core.transaction.HeadEntity;
+import com.jasonzqshen.familyaccounting.core.utils.CurrencyAmount;
 
 public class DocumentAccountIndexTester extends TesterBase {
-    private ReportsManagement _reportsManagement;
 
     @Override
     protected void doTest(CoreDriver coreDriver) throws Exception {
-        _reportsManagement = new ReportsManagement(coreDriver);
-
         // set root path
         coreDriver.setRootPath(TestUtilities.TEST_ACC_INDEX);
     }
 
     @Override
     protected void check(CoreDriver coreDriver) throws Exception {
-        DocumentIndex index = _reportsManagement
+        ReportsManagement rpMgmt = coreDriver.getReportsManagement();
+
+        DocumentIndex index = rpMgmt
                 .getDocumentIndex(DocumentIndex.ACCOUNT_INDEX);
         assertEquals(5, index.getKeys().size());
+
+        // check the cash account
         MasterDataIdentity_GLAccount glAccount2 = new MasterDataIdentity_GLAccount(
                 TestData.GL_ACCOUNT_CASH);
         DocumentIndexItem item = index.getIndexItem(glAccount2);
@@ -39,10 +41,39 @@ public class DocumentAccountIndexTester extends TesterBase {
         entities = item.getEntities(coreDriver.getStartMonthId(),
                 coreDriver.getStartMonthId());
         assertEquals(2, entities.size());
-        
+
         entities = item.getEntities(coreDriver.getCurMonthId(),
                 coreDriver.getCurMonthId());
         assertEquals(2, entities.size());
+
+        // check cash amount
+        assertEquals(new CurrencyAmount(-23.45), item.getAmount());
+        assertEquals(new CurrencyAmount(-23.45),
+                item.getAmount(coreDriver.getStartMonthId()));
+        assertEquals(new CurrencyAmount(0.00),
+                item.getAmount(coreDriver.getCurMonthId()));
+
+        // check cost account
+        MasterDataIdentity_GLAccount costAccount = new MasterDataIdentity_GLAccount(
+                TestData.GL_ACCOUNT_COST);
+        DocumentIndexItem costItem = index.getIndexItem(costAccount);
+        entities = costItem.getEntities();
+        assertEquals(3, entities.size());
+
+        entities = costItem.getEntities(coreDriver.getStartMonthId(),
+                coreDriver.getStartMonthId());
+        assertEquals(1, entities.size());
+
+        entities = costItem.getEntities(coreDriver.getCurMonthId(),
+                coreDriver.getCurMonthId());
+        assertEquals(2, entities.size());
+        
+        // check cost amount
+        assertEquals(new CurrencyAmount(123.45), costItem.getAmount());
+        assertEquals(new CurrencyAmount(123.45),
+                costItem.getAmount(coreDriver.getStartMonthId()));
+        assertEquals(new CurrencyAmount(0.00),
+                costItem.getAmount(coreDriver.getCurMonthId()));
     }
 
 }
